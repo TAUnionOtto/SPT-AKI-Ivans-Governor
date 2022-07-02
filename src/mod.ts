0e007ddff0fad5db4b5e08b369747da417090687
TAUnionEd <taunioned@msn.cn>
2022-07-02 09:45:53 +0800

    base: fix build scripts, add increase rag faur selling price

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,13 @@
 import { DependencyContainer } from "tsyringe";
 
-import { IMod } from "@spt-aki/models/external/mod";
-import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import type { IMod } from "@spt-aki/models/external/mod";
+import type { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import type { ConfigServer } from "@spt-aki/servers/ConfigServer";
+import type { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
+import type { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
+import type { IRagfairConfig } from "@spt-aki/models/spt/config/IRagfairConfig";
+
+import * as Enums from './enums';
 
 // 特殊装备
 // 尝试改变其背景色到橙色或暗红色，以表示其稀有度
@@ -13,22 +19,28 @@ const backpackId = '60a2828e8689911a226117f9';
 const gloveComponentId = '60a2828e8689911a226117f9'; // 需要一个可以打标签的箱子，当做背包的扩展，替代挎包
 
 class Mod implements IMod {
-  public load = () => { return; };
+  public load (container: DependencyContainer): void { 
+    this.increaseRagFairSellingPrice(container);
+  };
 
-  public delayedLoad = (container: DependencyContainer): void => {
-    this.unlockAllRSTItems(container);
+  public delayedLoad (container: DependencyContainer): void {
+    const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+    const dataBaseTables = databaseServer.getTables();
+
+    this.unlockAllRSTItems(dataBaseTables);
     // 大幅降低背包和杂物箱的掉率
     // 大幅降低机器人装备特殊装备的概率
     // 大幅提高特殊装备的价格，同时不得在跳蚤市场出售
 
-    // get globals settings and set flea market min level to be 1
-    // tables.globals.config.RagFair.minUserLevel = 1;
-}
+    dataBaseTables.globals.config.RagFair.minUserLevel = 1;
+  }
 
-private unlockAllRSTItems = (container: DependencyContainer): void => {
-    const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
-    const tables = databaseServer.getTables();
-    const items = tables.templates.items;
+  /**
+   * 解锁 BSG 黑名单
+   * @param dataBaseTables 
+   */
+  private unlockAllRSTItems (dataBaseTables: IDatabaseTables): void {
+    const items = dataBaseTables.templates.items;
     for (const itemId in items) {
         if (!Object.prototype.hasOwnProperty.call(items, itemId)) {
             continue;
@@ -37,7 +49,18 @@ private unlockAllRSTItems = (container: DependencyContainer): void => {
         currentItem._props.CanSellOnRagfair = true;
         currentItem._props.CanRequireOnRagfair = true;
     }
-}
+  }
+
+  /**
+   * 提高跳蚤市场物品售价
+   * @param container 
+   */
+  private increaseRagFairSellingPrice (container: DependencyContainer) {
+    const configServer = container.resolve<ConfigServer>("ConfigServer");
+    const regFairConfig = configServer.getConfig<IRagfairConfig>(Enums.ConfigTypes.RAGFAIR as unknown as ConfigTypes);
+    regFairConfig.dynamic.price.max = 4.5;
+    regFairConfig.dynamic.price.min = 2.25;
+  }
 }
 
 module.exports = { mod: new Mod() }
